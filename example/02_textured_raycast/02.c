09ccc96c52e466c8fa2039824db7892f5a6c4033
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-06-06 01:57:22 +0900

    learn 02_textured_raycast

---
+++ example/02_textured_raycast/02.c
@@ -0,0 +1,299 @@
+#include "../mlx/mlx.h"
+#include "key_macos.h"
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define X_EVENT_KEY_PRESS	2
+#define X_EVENT_KEY_EXIT	17
+#define texWidth 64
+#define texHeight 64
+#define mapWidth 24
+#define mapHeight 24
+#define width 1920
+#define height 1080
+
+typedef struct s_img
+{
+	void *img;
+	int *data;
+
+	int size_l;
+	int bpp;
+	int endian;
+}	t_img;
+
+typedef struct s_info
+{
+	double posX;
+	double posY;
+	double dirX;
+	double dirY;
+	double planeX;
+	double planeY;
+	void *mlx;
+	void *win;
+	t_img img;
+	int **buf;
+	int texture[8][texHeight * texWidth];
+	double moveSpeed;
+	double rotSpeed;
+} t_info;
+
+
+int	worldMap[24][24] = {
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+void	calc(t_info *info)
+{
+	int	x;
+
+	x = 0;
+	while (x < width)
+	{
+		double cameraX = 2 * x / (double)width - 1;//스크린 왼쪽 -1, 중앙 0, 오른쪽 1
+		double rayDirX = info->dirX + info->planeX * cameraX;
+		double rayDirY = info->dirY + info->planeY * cameraX;
+		//rayDir = 광선의 방향벡터
+		//광선의 방향은 (방향벡터) + (카메라평면 * 배수)로 구할 수 있음
+
+		//mapX,Y는 현재 광선의 위치, 광선이 있는 한 칸
+		int mapX = (int)info->posX;
+		int mapY = (int)info->posY;
+
+		//length of ray from current position to next x or y-side
+		//시작위치에서 처음 만나는 x, y의 면
+		double sideDistX;
+		double sideDistY;
+
+		//length of ray from one x or y-side to next x or y-side
+		//첫번째 x,y면에서 바로 다음 x,y면 (1만큼 이동)
+		double deltaDistX = fabs(1 / rayDirX);
+		double deltaDistY = fabs(1 / rayDirY);
+		double perpWallDist;//나중에 광선의 이동거리를 계산하는데 사용
+		//어안효과죽일때 사용함
+
+		//DDA알고리즘은 반복문 실행할 때마다 x, y방향으로 딱 한 칸씩 점프
+
+		//what direction to step in x or y-direction (either +1 or -1)
+		//광선의 방향에 따라 어느 방향으로 건너뛰는지 달라지는데 그 정보는 stepX,Y에 담김
+		int stepX;
+		int stepY;
+
+		int hit = 0; //was there a wall hit?
+		int side; //was a NS or a EW wall hit?
+
+		if (rayDirX < 0)
+		{
+			stepX = -1;
+			sideDistX = (info->posX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - info->posX) * deltaDistX;
+		}
+		if (rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (info->posY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - info->posY) * deltaDistY;
+		}
+		//hit 종료조건 벽의 x,y면과 부딪쳤는지 여부
+		while (hit == 0)
+		{
+			//jump to next map square, OR in x-direction, OR in y-direction
+			//x에 부딪혔
+			if (sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			//Check if ray has hit a wall
+			if (worldMap[mapX][mapY] > 0) hit = 1;
+		}
+		//어안효과 죽이기
+		if (side == 0)
+			perpWallDist = (mapX - info->posX + (1 - stepX) / 2) / rayDirX;
+		else
+			perpWallDist = (mapY - info->posY + (1 - stepY) / 2) / rayDirY;
+
+		//Calculate height of line to draw on screen
+		int lineHeight = (int)(height / perpWallDist);
+
+		//calculate lowest and highest pixel to fill in current stripe
+		int drawStart = -lineHeight / 2 + height / 2;
+		if(drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + height / 2;
+		if(drawEnd >= height)
+			drawEnd = height - 1;
+
+		int	color;
+		if (worldMap[mapY][mapX] == 1)
+			color = 0xFF0000;
+		else if (worldMap[mapY][mapX] == 2)
+			color = 0x00FF00;
+		else if (worldMap[mapY][mapX] == 3)
+			color = 0x0000FF;
+		else if (worldMap[mapY][mapX] == 4)
+			color = 0xFFFFFF;
+		else
+			color = 0xFFFF00;
+
+		if (side == 1)
+			color = color / 2;
+
+
+		for(int y = 0; y < height; y++)
+		{
+			if(y >= drawStart && y < drawEnd)
+				info->buf[y][x] = color;
+			else if(y < drawStart)
+				info->buf[y][x] = 0xb640ff;
+			else
+				info->buf[y][x] = 0x000000;
+		}
+
+		x++;
+	}
+}
+
+void draw(t_info *info)
+{
+	for(int y = 0; y < height; y++)
+	{
+		for(int x = 0; x < width; x++)
+		{
+			info->img.data[y * width + x] = info->buf[y][x];
+		}
+	}
+	mlx_put_image_to_window(info->mlx, info->win, info->img.img, 0, 0);
+}
+
+int	main_loop(t_info *info)
+{
+	calc(info);
+	draw(info);
+	// mlx_put_image_to_window(info->mlx, info->win, &info->img, 0, 0);
+
+	return (0);
+}
+
+int	key_press(int key, t_info *info)
+{
+	if (key == K_W)
+	{
+		if (!worldMap[(int)(info->posX + info->dirX * info->moveSpeed)][(int)(info->posY)])
+			info->posX += info->dirX * info->moveSpeed;
+		if (!worldMap[(int)(info->posX)][(int)(info->posY + info->dirY * info->moveSpeed)])
+			info->posY += info->dirY * info->moveSpeed;
+	}
+	//move backwards if no wall behind you
+	if (key == K_S)
+	{
+		if (!worldMap[(int)(info->posX - info->dirX * info->moveSpeed)][(int)(info->posY)])
+			info->posX -= info->dirX * info->moveSpeed;
+		if (!worldMap[(int)(info->posX)][(int)(info->posY - info->dirY * info->moveSpeed)])
+			info->posY -= info->dirY * info->moveSpeed;
+	}
+	//rotate to the right
+	if (key == K_D)
+	{
+		//both camera direction and camera plane must be rotated
+		double oldDirX = info->dirX;
+		info->dirX = info->dirX * cos(-info->rotSpeed) - info->dirY * sin(-info->rotSpeed);
+		info->dirY = oldDirX * sin(-info->rotSpeed) + info->dirY * cos(-info->rotSpeed);
+		double oldPlaneX = info->planeX;
+		info->planeX = info->planeX * cos(-info->rotSpeed) - info->planeY * sin(-info->rotSpeed);
+		info->planeY = oldPlaneX * sin(-info->rotSpeed) + info->planeY * cos(-info->rotSpeed);
+	}
+	//rotate to the left
+	if (key == K_A)
+	{
+		//both camera direction and camera plane must be rotated
+		double oldDirX = info->dirX;
+		info->dirX = info->dirX * cos(info->rotSpeed) - info->dirY * sin(info->rotSpeed);
+		info->dirY = oldDirX * sin(info->rotSpeed) + info->dirY * cos(info->rotSpeed);
+		double oldPlaneX = info->planeX;
+		info->planeX = info->planeX * cos(info->rotSpeed) - info->planeY * sin(info->rotSpeed);
+		info->planeY = oldPlaneX * sin(info->rotSpeed) + info->planeY * cos(info->rotSpeed);
+	}
+	if (key == K_ESC)
+		exit(0);
+	return (0);
+}
+
+int	main(void)
+{
+	t_info info;
+	info.mlx = mlx_init();
+
+	info.posX = 22.0;
+	info.posY = 11.5;
+	info.dirX = -1.0;
+	info.dirY = 0.0;
+	info.planeX = 0.0;
+	info.planeY = 0.66;
+	info.moveSpeed = 0.05;
+	info.rotSpeed = 0.05;
+
+	info.buf = (int **)malloc(sizeof(int *) * height);
+	for (int i = 0; i < height; i++)
+	{
+		info.buf[i] = (int *)malloc(sizeof(int) * width);
+	}
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			info.buf[i][j] = 0;
+		}
+	}
+
+	info.win = mlx_new_window(info.mlx, width, height, "mlx");
+
+	info.img.img = mlx_new_image(info.mlx, width, height);
+	info.img.data = (int *)mlx_get_data_addr(info.img.img, &info.img.bpp, &info.img.size_l, &info.img.endian);
+
+	mlx_loop_hook(info.mlx, &main_loop, &info);
+	mlx_hook(info.win, X_EVENT_KEY_PRESS, 0, &key_press, &info);
+
+	mlx_loop(info.mlx);
+}
